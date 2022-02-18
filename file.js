96915ba203d66be0fc156c17a465592ff511b883
Kisavi <deniskisavi@gmail.com>
2022-02-18 04:38:30 +0300

    perf - get city name input by user

--- file.js
+++ file.js
@@ -10,3 +10,14 @@ let lon;
 let lat;
 let inputtxt = document.querySelector("input")
 let correct = document.querySelector(".message-appended")
+
+//get info after entering city name
+inputtxt.addEventListener("keyup", checkInputValue)
+
+function checkInputValue(e) {
+    if (e.keyCode === 13 && inputtxt.value != "") {
+        requestApi(inputtxt.value)
+    } else if (inputtxt.value == "") {
+        window.location.reload()
+    }
+};
