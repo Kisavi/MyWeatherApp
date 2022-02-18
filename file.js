019930d2c7a6851f1890059c4c07f1e1452ff282
Kisavi <deniskisavi@gmail.com>
2022-02-18 04:36:30 +0300

    perf - declare the variables

---
+++ file.js
@@ -0,0 +1,12 @@
+//declaring variables
+let temperature = document.querySelector(".temperature");
+let description = document.querySelector(".description");
+let humidity = document.querySelector(".humidity");
+let wind = document.querySelector(".wind");
+let area = document.querySelector(".area");
+let weathericon = document.querySelector(".weathericon");
+const kelvin = 273;
+let lon;
+let lat;
+let inputtxt = document.querySelector("input")
+let correct = document.querySelector(".message-appended")
